85abd3493f61c6c63b001b028d969361ef570d4f
Sandesh Acharya <mysandesh@gmail.com>
2025-03-07 10:32:43 +0545

    Home screen layout changes

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,27 @@
 import { Stack } from "expo-router";
 
 const RootLayout = () => {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#7fffd4",
+        },
+        headerTintColor: "#000",
+        headerTitleStyle: {
+          fontSize: 20,
+          fontWeight: "bold",
+        },
+        contentStyle: {
+          paddingHorizontal: 10,
+          paddingTop: 10,
+          backgroundColor: "#f0f8ff",
+        },
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+    </Stack>
+  );
 };
 
 export default RootLayout;
--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,22 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import PostItImage from "@/assets/images/notepad.png";
+import { useRouter } from "expo-router";
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Edit app/index.jsx to edit this screen.</Text>
+      <Image source={PostItImage} style={styles.image} />
+      <Text style={styles.title}>Welcome to Work Logs.</Text>
+      <Text style={styles.subtitle}>
+        Log your work notes anytime, anywhere.
+      </Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => router.push("/notes")}
+      >
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -13,6 +26,39 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    padding: 20,
+    backgroundColor: "#f0f8ff",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#333",
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: "#666",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#a78bfa",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
 
