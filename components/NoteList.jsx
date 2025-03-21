3b541b9722394abf23eaeaeb5acafa4398bf9939
Sandesh Acharya <mysandesh@gmail.com>
2025-03-21 11:41:21 +0545

    NoteID fetch bugfix

--- components/NoteList.jsx
+++ components/NoteList.jsx
@@ -6,7 +6,7 @@ const NoteList = ({ notes }) => {
     <View>
       <FlatList
         data={notes}
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item) => item.$id}
         renderItem={({ item }) => <NoteItem note={item} />}
       />
     </View>
