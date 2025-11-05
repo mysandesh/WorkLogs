4d18744a396a29d4a6297a518c17713ac5176e90
Sandesh Acharya <mysandesh@gmail.com>
2025-11-05 20:52:43 +0545

    Auth Context added

---
+++ contexts/AuthContext.js
@@ -0,0 +1,24 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import authService from "../services/authService";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    checkUser();
+  }, []);
+
+  const checkUser = async () => {
+    setLoading(true);
+    const response = await authService.getUser();
+
+    if (response?.error) {
+      setUser(null);
+    } else {
+      setUser(response);
+    }
+    setLoading(false);
+  };
+};
