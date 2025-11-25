1b2b2718c72bc3cce2e2628137d7df12129b7786
Sandesh Acharya <mysandesh@gmail.com>
2025-11-25 11:19:21 +0545

    AuthProvider added to root layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,27 +1,30 @@
 import { Stack } from "expo-router";
+import { AuthProvider } from "@/contexts/AuthContext";
 
 const RootLayout = () => {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#7fffd4",
-        },
-        headerTintColor: "#000",
-        headerTitleStyle: {
-          fontSize: 20,
-          fontWeight: "bold",
-        },
-        contentStyle: {
-          paddingHorizontal: 10,
-          paddingTop: 10,
-          backgroundColor: "#f0f8ff",
-        },
-      }}
-    >
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="notes" options={{ headerTitle: "Notes" }} />
-    </Stack>
+    <AuthProvider>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#7fffd4",
+          },
+          headerTintColor: "#000",
+          headerTitleStyle: {
+            fontSize: 20,
+            fontWeight: "bold",
+          },
+          contentStyle: {
+            paddingHorizontal: 10,
+            paddingTop: 10,
+            backgroundColor: "#f0f8ff",
+          },
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+        <Stack.Screen name="notes" options={{ headerTitle: "Notes" }} />
+      </Stack>
+    </AuthProvider>
   );
 };
 
